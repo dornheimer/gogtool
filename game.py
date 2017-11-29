efda3ce158bd93b41bf3e483655722a9f185122f
dornheimer <beaveroftime@gmail.com>
2017-11-29 21:46:45 +0100

    moved functions from main to helper_functions and game_data

--- game.py
+++ game.py
@@ -2,7 +2,6 @@ import logging
 import os
 import subprocess
 
-
 logger = logging.getLogger(__name__)
 
 
--- lgog.py
+++ lgog.py
@@ -1,196 +1,24 @@
-import argparse
-from datetime import datetime
-import logging
-import json
 import os
 import subprocess
 import sys
 
 from download_directory import DownloadDir
-from game import Game
-
-
-def check_input(prompt, choices={'y', 'n'}):
-    """Verify user input."""
-    while True:
-        choice = input(prompt)
-        if choice.lower() in choices:
-            break
-        print(f'Invalid option: {choice}')
-
-    return choice
-
-
-def check_local_files(games_data, download_directory):
-    """Check files of every game and add to list if an update is available."""
-    local_games = []
-    for game in download_directory.games:
-
-        game_info = get_game_info(game, games_data)
-        local_path = download_directory.files[game]["local_path"]
-        local_files = download_directory.files[game]["setup_files"]
-        game_object = Game(game, game_info, local_path, local_files)
-        local_games.append(game_object)
-
-        logger.debug(f"Local files for {game}: {len(local_files)}")
-        if local_files == []:  # Empty folder
-            prompt = (f"Folder for {game} is empty. Download latest installer? (y/n) ")
-            if check_input(prompt) == "y":
-                game_object.update = True
-                game_object.conf = True
-
-    games_with_update = [lg for lg in local_games if lg.check_for_update()]
-    print("\nGames with outdated setup files:")
-    print("\n".join([g.name for g in games_with_update]), end="\n\n")
-
-    logger.debug(f"{len(games_with_update)} games with updates")
-    return games_with_update
-
-
-def get_games_details(data_path):
-    """Get game details from lgogdownloader json file."""
-    try:
-        with open(data_path) as data:
-            games_details = json.load(data)
-    except FileNotFoundError:
-        logger.error(f"Game details data not found in {data_path}", exc_info=True)
-        sys.exit()
-
-    logger.debug(f"Game details data found in {data_path}")
-    return games_details
-
-
-def is_outdated(games_details):
-    """Check if creation timestamp of game details is older than two days."""
-    logger.info("Checking games data creation date...")
-
-    gd_creation_date = datetime.strptime(games_details["date"], "%Y%m%dT%H%M%S")
-    gd_days_since_last_update = (datetime.now() - gd_creation_date).days
-    outdated = gd_days_since_last_update >= 2
-
-    str_outdated = "needs update" if outdated else "ok"
-    logger.debug("gamedetails.json created on: {}, age: {} days ({})".format(
-                gd_creation_date.strftime('%Y%m%d'), gd_days_since_last_update, str_outdated))
-
-    if outdated:
-        print("Games data is outdated. Updating...")
-    else:
-        print("Games data is up-to-date.")
-
-    return outdated
-
-
-def get_game_info(game, games_data):
-    """Check if folder name of game is actually known."""
-    for title in games_data:
-        if title['gamename'] == game:
-
-            logger.debug(f"Game info for {game} found")
-            return title
-
-    logger.warning(f"Game info for {game} not found")
-    return None
-
-
-def parse_command_line():
-    parser = argparse.ArgumentParser(prog="lgog")
-    parser.add_argument("--all", action="store_true",
-                        help="download newer versions for all games found in default directory")
-    parser.add_argument("--update", action="store_true",
-                        help="update game details cache")
-    parser.add_argument("--delete", action="store_true",
-                        help="delete outdated setup files after update")
-    parser.add_argument("--list", action="store_true",
-                        help="list outdated setup files")
-    parser.add_argument("--log", nargs="?", const="lgog.log",
-                        help="save output to log file (default='lgog.log')")
-    parser.add_argument("--debug", nargs="?", default="warning", const="info",
-                        choices=["info", "debug", "warning", "critical", "error", "notset"],
-                        help="print more information")
-    parser.add_argument("--platform", nargs="+", choices={'1', '2', '4'}, default={'4', '1'},
-                        help="set platform priority (default 1. 4=linux, 2. 1=windows)")
-    parser.add_argument("--directory", nargs="+",
-                        help="override default directory")
-    parser.add_argument("--clean", action="store_true",
-                        help="delete orphaned setup files")
-
-    return parser.parse_args()
-
-
-def parse_config(path, key=None):
-    """Return dictionary with options -> values from lgog config file."""
-    with open(path, 'r') as config_file:
-        lines = config_file.readlines()
-        config_dict = dict(line.strip().split(" = ") for line in lines)
-    if key:
-        try:
-            key_value = config_dict[key]
-            logger.debug(f"Succesfully parsed config file with key: '{key}'")
-            return key_value
-        except KeyError:
-            logger.error(f"Invalid key: '{key}'", exc_info=True)
-
-    logger.debug("Succesfully parsed config file")
-    return config_dict
-
-
-def setup_logging(args):
-
-    levels = {
-            "debug": logging.DEBUG,
-            "info": logging.INFO,
-            "warning": logging.WARNING,
-            "error": logging.ERROR,
-            "critical": logging.CRITICAL,
-            "notset": logging.NOTSET,
-            }
-
-    logging_level = levels.get(args.debug)
-
-    if args.log is not None:
-        # Set up logging to file
-        config_kwargs = {
-            "filename": args.log,
-            "filemode": "w",
-            "format": "%(asctime)s - %(levelname)s:%(name)s:[%(funcName)s]: %(message)s",
-            "datefmt": "%H:%M:%S",
-            "level": logging_level}
-    else:
-        # Log to console only
-        config_kwargs = {
-            "format": "%(levelname)s:%(name)s:[%(funcName)s]: %(message)s",
-            "level": logging_level}
-
-    logging.basicConfig(**config_kwargs)
-
-    if args.log is not None:
-        # Log to both console and file
-        console = logging.StreamHandler()
-        console.setLevel(logging_level)
-        formatter = logging.Formatter("%(levelname)s:%(name)s:[%(funcName)s]: %(message)s")
-        console.setFormatter(formatter)
-        logging.getLogger("").addHandler(console)
-
-    logger = logging.getLogger(__name__)
-    logger.debug(f"logger '{logger.name}' created")
-    return logger
+from game_data import GameData
+from helper_functions import *
 
 
 def main(args):
     logger.debug(f"Running with args: {args}")
 
-    games_details = get_games_details(DATA_PATH)
+    game_data = GameData(DATA_PATH)
 
-    # Automatically run update if games_details is outdated
-    if args.update or is_outdated(games_details):
+    # Automatically run update if game_data is outdated
+    if args.update or game_data.is_outdated:
         # Update game details cache
         logger.info("Running 'lgogdownloader --update-cache'...")
         update_cache = subprocess.Popen(["lgogdownloader", "--update-cache"], stdout=subprocess.PIPE)
         stdout, _ = update_cache.communicate()
         logger.info("Completed update")
-
-    # Get download directory from lgog config
-    games_data = games_details["games"]
 
     if args.directory:
         directory = os.path.abspath(args.directory)
@@ -204,7 +32,7 @@ def main(args):
         print("Download directory '{download_directory}' does not exist.")
         sys.exit()
 
-    games_with_update = check_local_files(games_data, download_directory)
+    games_with_update = check_local_files(game_data, download_directory)
 
     if args.clean:
         print("Cleaning outdated setup files...")
