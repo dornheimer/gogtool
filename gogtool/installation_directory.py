622f382fa8b536b4decbb880c2bac0aa9aec2f33
dornheimer <beaveroftime@gmail.com>
2017-12-12 10:51:32 +0100

    cleaned up _convert_title_format and added check for ':'

--- gogtool/installation_directory.py
+++ gogtool/installation_directory.py
@@ -26,30 +26,42 @@ class InstallDir(Directory):
         logger.info("Scanning for installed games...")
         dir_contents = os.listdir(self.path)
         for game in game_library.games.values():
-            game_name = game.gamename
-            install_name = self._convert_title_format(game.title)
-            install_name_alt = game_name.replace("_", "-")
-            if install_name in dir_contents or install_name_alt in dir_contents:
-                logger.debug(f"{install_name} found in installation directory")
-                install_path = os.path.join(self.path, game_name)
-                # Use "gamename" as identifier for consistency
-                self.installed_games_dict[game_name] = install_name, install_path
+            install_names = self._convert_title_format(game.title)
+
+            for iname in install_names:
+                if iname in dir_contents:
+                    logger.debug(f"{iname} found in installation directory")
+                    game_name = game.gamename
+                    install_path = os.path.join(self.path, game_name)
+                    # Use "gamename" as identifier for consistency
+                    self.installed_games_dict[game_name] = iname, install_path
 
     def initialize_game(self, game):
         if game.name in self.installed_games_dict:
             game.installed = True
             game.install_path = self.get_path(game.name)
 
-    # manage save games??
-    # connect to lutris??
+    # TODO: manage save games
+    # TODO: connect to lutris?
+
     def _convert_title_format(self, game_title):
-        # TODO: : is sometimes replaced with -
+        install_names = []
+
+        # Convert titles with special chars and replace consecutive spaces
+        # with a single space
         special_chars = re.compile(r"[^a-zA-Z0-9 ]")
         no_specials = special_chars.sub(" ", game_title)
         multiple_spaces = re.compile(r"\s{2,}")
         install_name = multiple_spaces.sub(" ", no_specials)
+        install_names.append(install_name)
 
-        return install_name
+        install_name_alt = game_title.replace("_", "-")
+        install_names.append(install_name_alt)
+
+        install_name_alt1 = game_title.replace(":", "-")
+        install_names.append(install_name_alt1)
+
+        return install_names
 
     def get_path(self, game_name):
         return self.installed_games_dict[game_name][1]
