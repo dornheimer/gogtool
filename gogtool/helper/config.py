f5af3392cc6b27e59ae362235348aa71eef34666
dornheimer <beaveroftime@gmail.com>
2018-01-18 22:45:51 +0100

    added type conversion to parse_config

--- gogtool/helper/config.py
+++ gogtool/helper/config.py
@@ -2,6 +2,8 @@
 
 from gogtool.helper.log import logger
 
+bool_dict = {'true': True, 'false': False}
+
 
 def parse_config(path, key=None):
     """Return dictionary with options -> values from lgog config file."""
@@ -11,10 +13,27 @@ def parse_config(path, key=None):
     if key:
         try:
             key_value = config_dict[key]
-            logger.debug(f"Succesfully parsed config file with key: '{key}'")
-            return key_value
         except KeyError:
             logger.error(f"Invalid key: '{key}'", exc_info=True)
+        else:
+            logger.debug(f"Succesfully parsed config file with key: '{key}'")
+            if is_int(key_value):
+                return int(key_value)
+            return bool_dict.get(key_value, key_value)
+
+    for key, value in config_dict.items():
+        if is_int(value):
+            config_dict[key] = int(value)
+        config_dict[key] = bool_dict.get(value, value)
 
     logger.debug("Succesfully parsed config file")
     return config_dict
+
+
+def is_int(string):
+    try:
+        int(string)
+    except ValueError:
+        return False
+    else:
+        return True
