4f27df9b40db44f246b48a18c88a8972c9ecf8e9
dornheimer <beaveroftime@gmail.com>
2018-01-18 22:34:02 +0100

    updated .gitignore with .pyc files

--- gogtool/helper/config.py
+++ gogtool/helper/config.py
@@ -2,6 +2,8 @@
 
 from gogtool.helper.log import logger
 
+bool_dict = {'true': True, 'false': False}
+
 
 def parse_config(path, key=None):
     """Return dictionary with options -> values from lgog config file."""
@@ -12,9 +14,20 @@ def parse_config(path, key=None):
         try:
             key_value = config_dict[key]
             logger.debug(f"Succesfully parsed config file with key: '{key}'")
-            return key_value
+            if is_int(key_value):
+                return int(key_value)
+            return bool_dict.get(key_value, key_value)
         except KeyError:
             logger.error(f"Invalid key: '{key}'", exc_info=True)
 
     logger.debug("Succesfully parsed config file")
     return config_dict
+
+
+def is_int(string):
+    try:
+        int(string)
+    except ValueError:
+        return False
+    else:
+        return True
