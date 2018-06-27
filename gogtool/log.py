046a585bf94647823aa334c49141b88968881991
dornheimer <beaveroftime@gmail.com>
2018-06-27 14:02:17 +0200

    prevent adding duplicate handlers, change log format

--- gogtool/log.py
+++ gogtool/log.py
@@ -19,19 +19,21 @@ def configure_logger(log_level='debug', log_file=None):
         file_handler = logging.handlers.RotatingFileHandler(
             log_file, maxBytes=1048576, backupCount=1)
 
-        file_format = "[%(levelname)-6s:%(name)s:%(funcName)s]: %(message)s"
+        file_format = "[%(levelname)-6s:%(name)s:%(funcName)s] %(message)s"
         file_formatter = logging.Formatter(file_format)
         file_handler.setFormatter(file_formatter)
 
-        file_handler.setLevel(logging.DEBUG)
-        logger.addHandler(file_handler)
-
     console = logging.StreamHandler()
-    console_format = "%(levelname)-8s[%(module)s:%(funcName)s]: %(message)s"
+    console_format = "%(levelname)-8s[%(module)s:%(funcName)s] %(message)s"
     date_fmt = "%H:%M:%S"
     console_formatter = logging.Formatter(console_format, date_fmt)
     console.setFormatter(console_formatter)
     console.setLevel(LOG_LEVELS[log_level])
-    logger.addHandler(console)
+
+    # Prevent adding duplicate handlers
+    if not len(logger.handlers):
+        if log_file is not None:
+            logger.addHandler(file_handler)
+        logger.addHandler(console)
 
     return logger
